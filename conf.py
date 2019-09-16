286610deb9046c8c8d5b10f4ab4ad712c499526d
Lazar Bojičić <lazar.bojicic42@gmail.com>
2019-09-16 14:41:56 +0200

    publish adaptation
    Changed conf.py to work with petljadoc
    Chnaged copyright templates
    Consistantnly changed titles
    Removed .rst files in Serbian
    Ran petljadoc publish command to create docs folder

--- conf.py
+++ conf.py
@@ -119,7 +119,7 @@ html_theme = 'custom_theme'
 #html_theme_options = {'nosidebar': 'true'}
 html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "Scratch_eng",
+    'navbar_title': "Block-based programming in Scratch - practice lessons",
 
     # Tab name for entire site. (Default: "Site")
     'navbar_site_name': "Chapters",
@@ -165,10 +165,10 @@ html_theme_path = ["_templates/plugin_layouts"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Runestone Interactive Overview'
+html_title = 'Block-based programming in Scratch - practice lessons'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title ='Runestone Interactive Overview'
+html_short_title ='Block-based programming in Scratch - practice lessons'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
